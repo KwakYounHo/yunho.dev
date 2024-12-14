8b2d0de464895ace985d91b6c4392148560f860e
곽윤호 <kwakdm95@gmail.com>
2024-12-14 19:33:22 +0900

    feat: NextJS 설정 및 초기 페이지 설정 - 설정: 1. 빌드 결과물 독립형으로 설정 2. 외부 API 프록시 설정

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  async rewrites() {
+    const apiServerUrl =
+      process.env.NODE_ENV === "production"
+        ? `http://api.${process.env.DOMAIN_NAME}`
+        : "https://localhost:8000";
+    return [
+      {
+        source: "/external_api/:path*",
+        destination: `${apiServerUrl}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

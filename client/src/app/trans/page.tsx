ad213b28188df369ad3c2dc468420f70473f6da5
곽윤호 <kwakdm95@gmail.com>
2024-12-15 23:43:04 +0900

    chore: 줄바꿈

--- client/src/app/trans/page.tsx
+++ client/src/app/trans/page.tsx
@@ -3,9 +3,11 @@ import { API_SERVER_URL } from "@/utils/apiServer";
 
 const TransPortationTest = async () => {
   const response = await fetch(`${API_SERVER_URL}/`);
+
   if (response.status >= 400) {
     return <div>Error</div>;
   }
+
   const data: GetHelloResponse = await response.json();
 
   return <div>{data.message}</div>;

b05067f3d670183c8485dc28d0334899d3982c0f
곽윤호 <kwakdm95@gmail.com>
2024-12-20 00:54:33 +0900

    fix: 경로 이슈

--- client/src/utils/apiServer/address.ts
+++ client/src/utils/apiServer/address.ts
@@ -1,4 +1,4 @@
 export const API_SERVER_URL =
   process.env.NODE_ENV === "production"
-    ? `https://api.${process.env.DOMAIN_NAME}`
+    ? `https://api.${process.env.DOMAIN_NAME}/api`
     : "http://proxy-dev/api";

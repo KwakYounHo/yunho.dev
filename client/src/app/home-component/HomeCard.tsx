38030c83074fdd0194ace5e66d77574dabff2485
곽윤호 <kwakdm95@gmail.com>
2024-12-27 18:07:42 +0900

    chore: 임시 폐쇄

--- client/src/app/home-component/HomeCard.tsx
+++ client/src/app/home-component/HomeCard.tsx
@@ -4,12 +4,12 @@ import Impressive from "./Impressive";
 const HomeCard = () => {
   return (
     <div className="flex flex-col items-center text-center gap-4">
-      <Link href="/lyrics">
-        <Gravatar
-          size={400}
-          className="md:w-52 md:h-52 lg:w-72 lg:h-72 w-48 h-48 hover:scale-105 transition-all duration-300 shadow-xl shadow-foreground/20"
-        />
-      </Link>
+      {/* <Link href="/lyrics"> */}
+      <Gravatar
+        size={400}
+        className="md:w-52 md:h-52 lg:w-72 lg:h-72 w-48 h-48 hover:scale-105 transition-all duration-300 shadow-xl shadow-foreground/20"
+      />
+      {/* </Link> */}
       <div className="flex flex-col items-center text-center gap-2">
         <div>
           <h1 className="xl:text-6xl text-4xl font-bold">Juno</h1>
